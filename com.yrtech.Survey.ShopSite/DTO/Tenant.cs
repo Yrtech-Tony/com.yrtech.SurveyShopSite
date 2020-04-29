5e9532549ec7558fc78cc67a42223f865fe7051d
huohaitao1 <huohaitao1@126.com>
2020-04-29 23:44:00 +0800

    HHT-增加DTO

---
+++ com.yrtech.Survey.ShopSite/DTO/Tenant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.yrtech.Survey.ShopSite.DTO
+{
+    public class Tenant
+    {
+        public int TenantId { get; set; }
+        public string TenantCode { get; set; }
+        public string TenantName { get; set; }
+        public Nullable<int> InUserId { get; set; }
+        public Nullable<System.DateTime> InDateTime { get; set; }
+        public Nullable<int> ModifyUserId { get; set; }
+        public Nullable<System.DateTime> ModifyDateTime { get; set; }
+    }
+}
